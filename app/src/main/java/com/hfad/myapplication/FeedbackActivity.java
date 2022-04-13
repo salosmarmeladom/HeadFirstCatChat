fa1cd099573468216e7b7ed1ee4a6acd5f1825f3
salosmarmeladom <pollyklaps@outlook.com>
2022-04-13 19:01:27 +0300

    Initial commit

---
+++ app/src/main/java/com/hfad/myapplication/FeedbackActivity.java
@@ -0,0 +1,17 @@
+package com.hfad.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(android.graphics.Color.WHITE);
+        setSupportActionBar(toolbar);
+    }
+}
